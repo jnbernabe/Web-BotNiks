9cc38dd26c5fd022427cb5930c4db4f92a86c325
Jamaal Bernabe <jnbernabe@gmail.com>
2021-11-10 12:10:20 -0500

    WB-2 Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { HeaderComponent } from './partials/header/header.component';
+import { FormComponent } from './partials/form/form.component';
+import { LoginComponent } from './partials/login/login.component';
+import { RegisterComponent } from './partials/register/register.component';
+import { TableComponent } from './partials/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, FooterComponent, HeaderComponent, FormComponent, LoginComponent, RegisterComponent, TableComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

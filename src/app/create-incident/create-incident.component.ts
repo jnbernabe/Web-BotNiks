6ef757feac4ca38139507d61a6717ed25aa098e5
Charlie <charlie_bi@hotmail.com>
2021-11-12 10:43:20 -0500

    Linked Enums for priority and ptatus to the create incident form componenet to be auto generated

--- src/app/create-incident/create-incident.component.ts
+++ src/app/create-incident/create-incident.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Priority } from '../model/priority.enum';
+import { Status } from '../model/status.enum';
 
 @Component({
   selector: 'app-create-incident',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateIncidentComponent implements OnInit {
 
+  priority = Priority;
+  priorities(): Array<string> {
+    var keys = Object.keys(this.priority);
+    return keys;
+  }
+
+  status = Status;
+  statuses(): Array<string> {
+    var status = Object.keys(this.status);
+    return status;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }

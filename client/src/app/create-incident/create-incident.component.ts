d5b91c830ab815c84da289060b5b6db357d8effc
Charlie <charlie_bi@hotmail.com>
2021-11-13 20:47:56 -0500

    partially implemented the edit incident logic using the new-incident template

--- client/src/app/create-incident/create-incident.component.ts
+++ client/src/app/create-incident/create-incident.component.ts
@@ -8,6 +8,16 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { HttpServiceService } from '../config/http-service.service';
 
 let counter = 1;
+let isEdit = false;
+let id = '';
+let prio = Priority[2];
+let stat = Status[1];
+let fName = '';
+let lName = '';
+let email = '';
+let phone = '';
+let desc = '';
+let narrative = '';
 
 @Component({
   selector: 'app-create-incident',
@@ -16,6 +26,7 @@ let counter = 1;
 })
 export class CreateIncidentComponent implements OnInit {
   incidentId = this.createTicketNumber();
+  editMode = isEdit;
 
   //for listing all elements of priority enum as an option in the html forms
   priority = Priority;
@@ -32,17 +43,17 @@ export class CreateIncidentComponent implements OnInit {
   }
   //Form Group
   incidentForm = new FormGroup({
-    incidentNumber: new FormControl(this.incidentId),
-    incidentPriority: new FormControl(''),
-    incidentStatus: new FormControl(Status[1]),
-    customerFName: new FormControl(''),
-    customerLName: new FormControl(''),
-    customerEmail: new FormControl(''),
-    customerPhone: new FormControl(''),
-    incidentDescription: new FormControl(''),
-    incidentNarrative: new FormControl(''),
+    incidentNumber: new FormControl(id),
+    incidentPriority: new FormControl(prio),
+    incidentStatus: new FormControl(stat),
+    customerFName: new FormControl(fName),
+    customerLName: new FormControl(lName),
+    customerEmail: new FormControl(email),
+    customerPhone: new FormControl(phone),
+    incidentDescription: new FormControl(desc),
+    incidentNarrative: new FormControl(narrative),
   });
-
+  //creates a string of numbers using date and incrementer
   createTicketNumber(): string {
     let d = new Date();
     let dateNumber =
@@ -54,13 +65,13 @@ export class CreateIncidentComponent implements OnInit {
     console.log(dateNumber);
     return dateNumber;
   }
-
+  //creates an incident using values provided in the form
   createIncident(): Incident {
     let customer = new Customer(
-      this.incidentForm.get('customerFName')?.value,
-      this.incidentForm.get('customerLName')?.value,
-      this.incidentForm.get('customerEmail')?.value,
-      this.incidentForm.get('customerPhone')?.value
+      this.incidentForm?.get('customerFName')?.value,
+      this.incidentForm?.get('customerLName')?.value,
+      this.incidentForm?.get('customerEmail')?.value,
+      this.incidentForm?.get('customerPhone')?.value
     );
 
     let user = new User(
@@ -75,18 +86,19 @@ export class CreateIncidentComponent implements OnInit {
     let dateCreated = new Date();
 
     let obj = new Incident(
-      this.incidentForm.get('incidentNumber')?.value,
-      this.incidentForm.get('incidentPriority')?.value,
-      this.incidentForm.get('incidentStatus')?.value,
+      this.incidentForm?.get('incidentNumber')?.value,
+      this.incidentForm?.get('incidentPriority')?.value,
+      this.incidentForm?.get('incidentStatus')?.value,
       user,
       dateCreated,
-      this.incidentForm.get('incidentDescription')?.value,
-      this.incidentForm.get('incidentNarrative')?.value,
+      this.incidentForm?.get('incidentDescription')?.value,
+      this.incidentForm?.get('incidentNarrative')?.value,
       customer
     );
     console.log(obj);
-    console.log(obj.Status)
-    console.log(obj.Priority)
+    console.log(obj.Status);
+    console.log(obj.Priority);
+
     let jsonobj = JSON.stringify(obj);
     console.log(jsonobj);
 
@@ -95,7 +107,42 @@ export class CreateIncidentComponent implements OnInit {
     return obj;
   }
 
+  //populates form if object exists
+  private initForm() {
+    if (isEdit == true) {
+      //dummy object
+      let obj = new Incident(
+        '121211-01',
+        'High',
+        'InProgress',
+        new User(
+          'John',
+          'Doe',
+          'email@rogers.com',
+          'Desursdfauo',
+          '5467778248',
+          'Admin'
+        ),
+        new Date(),
+        'Short Description',
+        'Short Narrative',
+        new Customer('Han', 'Bi', 'example@hotmail.com', '4165255677')
+      );
+      this.incidentForm.get('incidentNumber')?.setValue(obj.id);
+      this.incidentForm.get('incidentPriority')?.setValue(obj.Priority);
+      this.incidentForm?.get('incidentStatus')?.setValue(obj.Status);
+      this.incidentForm?.get('customerFName')?.setValue(obj.Customer.FName);
+      this.incidentForm?.get('customerLName')?.setValue(obj.Customer.LName);
+      this.incidentForm?.get('customerEmail')?.setValue(obj.Customer.Email);
+      this.incidentForm?.get('customerPhone')?.setValue(obj.Customer.Number);
+      this.incidentForm?.get('incidentDescription')?.setValue(obj.Description);
+      this.incidentForm?.get('incidentNarrative')?.setValue(obj.Narrative);
+    }
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initForm();
+  }
 }
--- client/src/app/model/priority.enum.ts
+++ client/src/app/model/priority.enum.ts
@@ -1,5 +1,5 @@
 export enum Priority {
-  Medium = 0,
   Low = 1,
-  High = 2,
+  Medium = 2,
+  High = 3,
 }

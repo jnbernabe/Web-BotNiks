a8790b2b03814007ffe1a62db1f8a348396f3acb
Charlie <charlie_bi@hotmail.com>
2021-11-14 15:44:12 -0500

    added a counter as a service

---
+++ client/src/app/model/incident-number-generator.model.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'any',
+})
+export class IncidentNumberGenerator {
+  public counter = 0;
+  getCounter(): number {
+    this.counter++;
+    return this.counter;
+  }
+
+  ngOnInit(): void {}
+}

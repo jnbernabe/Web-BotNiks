590c064f7efbd1286feb452c3475139b85012342
hetushiroya <hetushiroya@gmail.com>
2021-11-20 01:33:46 -0500

    login authentication

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { BasePageComponent } from 'src/app/partials/basepage/basepage.component';
 
@@ -8,9 +11,30 @@ import { BasePageComponent } from 'src/app/partials/basepage/basepage.component'
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent extends BasePageComponent implements OnInit {
-  constructor(route: ActivatedRoute) {
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(route: ActivatedRoute,private auth: AuthService, private router: Router) {
     super(route);
   }
 
   override ngOnInit(): void {}
-}
+
+  onSubmit(): void{
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe(
+        (result) => {
+          console.log(result);
+          this.router.navigate(['/home']);
+        },
+        (err: Error) => {
+          alert(err.message);
+        }
+      );
+    }
+  }
+  }
+
